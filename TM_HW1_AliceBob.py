107d7d9ac62987f69e18632def03c9aa75df14c7
Yusuf Akçay <yusufakcay.baa@gmail.com>
2022-04-15 23:21:53 +0300

    Minor arrangements

--- TM_HW1_AliceBob.py
+++ TM_HW1_AliceBob.py
@@ -1,5 +1,7 @@
 def compareTriplets(a,b):
-    points=[0,0]
+    points=[0,0] #initialize scores
+
+    #check for all inputs
     for i in range(3):
         if a[i]>b[i]:
             points[0]+=1
@@ -7,7 +9,7 @@ def compareTriplets(a,b):
             points[1]+=1
     print(points[0],points[1])
 
-alice=list(map(int,input().split()))
-bob=list(map(int,input().split()))
+alice=list(map(int,input().split())) #Get alice inputs
+bob=list(map(int,input().split())) #Get bob inputs
 
 compareTriplets(alice,bob)
